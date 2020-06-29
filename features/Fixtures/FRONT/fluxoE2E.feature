3d04cc2fe3c21cd888df28f8925821c41426ea6a
Alisson da Costa <alisson.santos@webjump.com.br>
2020-06-29 18:00:49 -0300

    WIP for E2E

--- features/Fixtures/FRONT/fluxoE2E.feature
+++ features/Fixtures/FRONT/fluxoE2E.feature
@@ -3,9 +3,10 @@ Funcionalidade: Acesso a page para realizar um end to end.
 @e2e
     Cenario: Finalizar uma compra com o usuario luma
         Dado que estou navegando na 'URL'
-        E realizo acesso com 'user_luma'
+        E realizo acesso com 'roni_cost@example.com' e 'roni_cost3@example.com'
         Quando adiciono o item 'Advanced Pilates & Yoga (Strength)' ao carrinho
         E avanço para o checkout
+        E preencho os dados de Shipping
         E finalizo a compra
         Então vejo meu numero de pedido
 
--- features/step_definitions/e2e.rb
+++ features/step_definitions/e2e.rb
@@ -2,16 +2,38 @@ Dado('que estou navegando na {string}') do |url|
     visit 'https://magento.nublue.co.uk/'
 end
 
-Dado('realizo acesso com {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Dado('realizo acesso com {string} e {string}') do |user, pass|
+    click_link_or_button 'Sign In'
+    find("#email").set user
+    find("#pass").set pass
+    click_button 'Sign In'
+    assert_text 'Veronica Costello'
 end
 
-Quando('adiciono o item {string} ao carrinho') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Quando('adiciono o item {string} ao carrinho') do |item_search|
+  find("#search").set item_search
+  click_button 'Search'
+  first(".product-item-link").click
+  find("#option-label-size-142-item-168").click
+  find("#option-label-color-93-item-52").click
+  find("#qty").set 2
+  click_button 'product-addtocart-button'
 end
 
 Quando('avanço para o checkout') do
-  pending # Write code here that turns the phrase above into concrete actions
+    click_link 'shopping cart', wait: 2
+    find(".action primary checkout").click :count => 4
+end
+
+Quando('preencho os dados de Shipping') do
+    find("#T305KQH").set FFaker::AddressIN.street_address, :count => 4
+    find("#Q4CO18R").set FFaker::AddressIN.city
+    find("#G7S66Q0").set FFaker::AddressIN.state
+    find("#RCC1CIW").set FFaker::AddressIN.zip_code
+    select 'GIJ4DPB', from: 'Maldives'
+    find("#RV7XG2B").set FFaker::PhoneNumberAU.international_mobile_phone_number
+    click_button 'Next'
+    sleep 2
 end
 
 Quando('finalizo a compra') do
