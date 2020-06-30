97d5e5e342b27b06cfc17a11ca4fe780208102bd
Alisson da Costa <alisson.santos@webjump.com.br>
2020-06-30 17:39:49 -0300

    Clean Cookies

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,6 @@
 
-# Capybara.current_session.driver.browser.manage.delete_all_cookies
-# page.driver.browser.manage.window.resize_to(2000, 1600)
+Before do
+    Capybara.current_session.driver.browser.manage.delete_all_cookies
+    page.driver.browser.manage.window.resize_to(1440, 900)
+end
+
